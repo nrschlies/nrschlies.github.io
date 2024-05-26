286777376341193c5c651fbb3618773ead2c5f1b
Noah Schliesman <nrschlies@gmail.com>
2024-05-26 11:44:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'css/styles.css';
-import App from './App';
+import App from './js/App';
 
 ReactDOM.render(
   <React.StrictMode>
