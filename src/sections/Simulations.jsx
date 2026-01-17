317961d6077ea293d50126f36e7a7ee6a3b314a4
Noah Schliesman <nschliesman@sandiego.edu>
2026-01-16 22:38:18 -0800

    Fixed type for sim page

--- src/sections/Simulations.jsx
+++ src/sections/Simulations.jsx
@@ -2,7 +2,7 @@ function Simulations() {
   return (
     <section>
       <h2>Simulations</h2>
-      <p>This site is currently under a major overhaul. Readings will be ported in the coming weeks.</p>
+      <p>This site is currently under a major overhaul. Simulations will be ported in the coming weeks.</p>
     </section>
   );
 }
