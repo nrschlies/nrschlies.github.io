7d578d1f399b34790b1c27f06c7211be73162edb
Noah Schliesman <nschliesman@sandiego.edu>
2026-01-16 21:51:41 -0800

    modified vite config and yml for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',      // keep this for a user site like nrschlies.github.io
   plugins: [react()],
 })
